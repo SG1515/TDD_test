b8eccf465cd164dee7a4c92c56f85a9d63a0e573
SG1515 <sunggu507@naver.com>
2022-07-05 13:31:40 +0900

    테스트 케이스 2개, 더하기 기능

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -6,8 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test
-    public void 더하기_테스트 (){
-        int a = 10 + 5;
-        assertEquals(15, a);  //단정문 (기대하는 값, 결과)
+    public void 더하기_테스트_2 (){
+        int rs  = Calculator.add("20 + 20");
+
+        assertEquals(40, rs);
     }
 }
